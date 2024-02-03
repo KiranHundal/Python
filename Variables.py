75b3d806e7cf1d87b2771f842f4fb48230a3909b
KiranHundal <hundalkiran6@gmail.com>
2024-02-03 14:44:25 -0800

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,60 @@
+# Variables:
+#  boxes with names that hold one value at a time.
+#  Variables are the programming language's memory.
+#  You put a value inot a Variable by setting the variable to a value or assigning a value to the variable.
+#  You can later look at the vlaue in the variable and use it.
+# If you set/assign a new value to the variable, then the old value goes away.
+
+# 1 Storing a value in a variable
+# 2 Changing the value of a variable based on the value stored in it
+# 3 Telling the computer to do stuff
+# 4 Telling the computer to do something, if something is true
+# 5 Using the dot . to tell things called "objects" to do stuff
+# 6 How to do the same thing over and over
+
+
+# 1:
+persons_name = "Gurpreet"
+persons_name = "Amrit"
+# 2:
+persons_name = "Kiran"
+
+
+
+# 1:
+age = 19
+age = 21
+age = 25
+# 2:
+age = 15
+
+
+# "Amrit" = string
+# 21 = ini(integers)
+
+# 3:
+# print(persons_name, age)
+
+# 4:
+# if age is greater than 18
+# if age > 18:
+# print: you are older than 18.
+    # print("You are older than 18.")
+# else:
+    # print("You are not 18 years old.")
+
+# 5: Dot .
+
+message = "This is a string."
+number = 20
+# print("I want to print", message)
+# The number is 1928
+# print("I want to print", number)
+
+# if number > 15:
+#     print("The number is greater than 15")
+
+if number < 15:
+    print("The number is less than 15")
+else:
+    print("The number is greater than 15")
